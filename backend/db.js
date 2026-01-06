4a11b19139548e68b6e8798cc327c71db4c17bbf
kavin53 <kavin53>
2026-01-06 10:25:05 +0530

    database connect

---
+++ backend/db.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'bugbounty_lab'
+});
+
+db.connect(err =>{
+    if(err) throw err;
+    console.log('MySQL Connected...');
+});
+module.exports = db;
