a7bba625e45929f6dbbc3abe749e1a975556f4cd
kavin53 <jayarathnekavindu510@gmail.com>
2026-01-06 11:39:06 +0530

    updates

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,26 @@ const express = require('express');
 const cors = require('cors');
 const authRoutes = require('./routes/auth');
 const labRoutes = require('./routes/lab');
+const path = require('path');
+
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/auth', authRoutes);
-app.use('/api/labs', labRoutes);
+// 🔹 Serve frontend files
+app.use(express.static(path.join(__dirname, '../frontend')));
 
-app.listed(3000,()=>{
+// 🔹 Homepage → index.html
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/index.html'));
+});
+
+// 🔹 API routes
+app.use('/api/auth', authRoutes);
+app.use('/api/lab', labRoutes);
+
+app.listen(3000, () => {
     console.log('Server running on http://localhost:3000');
 });
