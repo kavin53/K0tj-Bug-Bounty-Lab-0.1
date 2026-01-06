7c353ea866ea54efd5ef21021578a4ec0daa443f
kavin53 <jayarathnekavindu510@gmail.com>
2026-01-06 20:13:57 +0530

    error fixed when sql injection

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -8,6 +8,13 @@ router.post('/login',(req, res) =>{
     const query = `select * from users where username ='${username}' and password = '${password}'`;
 
     db.query(query,(err,result) =>{
+
+        if(err){
+            console.error("SQL ERROR",err.message);
+
+            return res.json({success: false, error:"SQL error"})
+        }
+
         if(result.length>0){
             res.json({ success: true});
         }else {
