867fea8edb9903613f43ceb8bb5a9e257cbb663a
kavin53 <kavin53>
2026-01-06 09:46:53 +0530

    base create

---
+++ database/schema.sql
@@ -0,0 +1,24 @@
+create database bugbounty_lab;
+use bugbounty_lab;
+
+create table users(
+    id int auto_increment primary key,
+    username varchar(50),
+    password varchar(50)
+);
+
+create table labs(
+    id int auto_increment primary key,
+    title varchar(100),
+    level varchar(20),
+    description text        
+);
+
+INSERT INTO users (username, password)
+VALUES ('admin', 'admin123');
+
+INSERT INTO labs (title, level, description)
+VALUES
+('SQL Injection', 'Beginner', 'Basic login SQLi'),
+('Stored XSS', 'Pro', 'Stored cross-site scripting'),
+('IDOR', 'Elite', 'Insecure direct object reference');
