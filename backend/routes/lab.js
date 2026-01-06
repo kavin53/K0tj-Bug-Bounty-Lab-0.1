44aee512928bc60855251124ac425cd0a986db31
kavin53 <jayarathnekavindu510@gmail.com>
2026-01-06 10:56:57 +0530

    set lab.js

---
+++ backend/routes/lab.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const db =require('/db');
+
+router.get('/',(req,res) =>{
+    db.query("select * from labs", (err, results) =>{
+        res.json(results);
+    });
+});
+
+module.exports = router;
