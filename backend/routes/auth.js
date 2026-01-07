8ce611b8f5bfc34e508161819f09b79f6a40372b
kavin53 <jayarathnekavindu510@gmail.com>
2026-01-07 13:26:24 +0530

    token validation bug create

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../db');
+const crypto = require('crypto');
 
 router.post("/login-vuln",(req, res) =>{
     const{ username, password} = req.body;
@@ -65,4 +66,39 @@ router.post('/login-blind',(req, res) =>{
     });
 });
 
+router.post('/forget-password',(req,res) =>{
+    const {email} = req.body;
+
+    const token = crypto .randomBytes(20).toString('hex');
+    const expires = Date.now() + 15*60*1000 ; 
+
+    const sql = 
+`UPDATE users SET reset_token = ?, reset_expires = ? WHERE email = ?`;
+
+    db.query(sql,[token, expires, email], (err) =>{
+        res.json({message:"If that email address is in our database, we will send you an email to reset your password."});
+
+        console.log(`Password reset link: http://example.com/reset-password?token=${token}`);
+    });
+});
+
+router.post('/reset-password',(req,res) =>{
+    const {token, newPassword} = req.body;
+
+    const sql =
+`SELECT * FROM users WHERE reset_token = ? AND reset_expires > ?`;
+
+    db.query(sql,[token, Date.now()], (err, result) =>{
+        if(result.length === 0) {
+            return res.status(400).json({message:"Invalid or expired token"});
+        }
+
+        const updateSql = `
+        update users set password=? where reset_token=?`;
+
+        db.query(updateSql,[newPassword, token], () =>{
+            res.json({message:"Password has been reset successfully"});
+        });
+    });
+});      
 module.exports = router;
