c27b94ed57b9867f6315261cc67edf76995e31e3
kavin53 <jayarathnekavindu510@gmail.com>
2026-01-08 12:26:04 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -11,15 +11,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// 🔹 Serve frontend files
+
 app.use(express.static(path.join(__dirname, '../frontend')));
 
-// 🔹 Homepage → index.html
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend/index.html'));
 });
 
-// 🔹 API routes
+
 app.use('/api/auth', authRoutes);
 app.use('/api/lab', labRoutes);
 app.use('/api/reset-vuln', resetVuln);
