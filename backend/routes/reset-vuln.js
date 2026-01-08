a322f7e06c4e4d553d2405e23e754a0149474353
kavin53 <jayarathnekavindu510@gmail.com>
2026-01-08 12:26:21 +0530

    update to email

--- backend/routes/reset-vuln.js
+++ backend/routes/reset-vuln.js
@@ -3,40 +3,45 @@ const router = express.Router();
 const db = require('../db');
 const crypto = require('crypto');
 
+
 router.post('/request', (req, res) => {
-    const{ email } = req.body;
+    const { email } = req.body;
+
     const token = crypto.randomBytes(16).toString('hex');
-    const expires = Date.now() + 10*60*1000;
-    
-    console.log(token, expires);
+    const expires = Date.now() + 10 * 60 * 1000;
 
+    db.query(
+        "UPDATE users SET reset_token=?, reset_expires=? WHERE email=?",
+        [token, expires, email],
+        () => {
+            res.json({
+            });
+        }
+    );
+});
 
-    const sql = `
-        update users set reset_token = ?, reset_expires = ? where email = ? `;
-
-    db.query(sql, [token, expires, email], () => {
-        res.json({
-            reset_link: `http://localhost:3000/reset-password.html?token=${token}`
-        });
-    });
-}); 
 
 router.post('/reset', (req, res) => {
-    const {token,password} = req.body;
+    const { token, password } = req.body;
 
-    const sql = `select * from users where reset_token = ? and reset_expires >?`;
+    db.query(
+        "SELECT * FROM users WHERE reset_token=? AND reset_expires>?",
+        [token, Date.now()],
+        (err, result) => {
 
-    db.query(sql, [token, Date.now()], (err, result) => {
-        if(result.length === 0) {
-            return res.status(400).json({message: "Invalid or expired token"});
+            if (result.length === 0) {
+                return res.json({ message: "Invalid token" });
+            }
+
+            
+            db.query(
+                "UPDATE users SET password=? WHERE id=?",
+                [password, result[0].id]
+            );
+
+            res.json({ message: "Password reset success" });
         }
-
-        const update = `update users set password = ? where reset_token = ?`;
-
-        db.query(update, [password, token], () => {
-            res.json({message: "Password has been reset successfully"});
-        });
-    });
+    );
 });
 
 module.exports = router;
