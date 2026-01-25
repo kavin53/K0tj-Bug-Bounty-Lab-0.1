c5bc63c6d77d6bb6316ac41f7c97a87eb0e19ab3
kavin53 <jayarathnekavindu510@gmail.com>
2026-01-25 22:27:06 +0530

    add session checking

--- frontend/js/auth.js
+++ frontend/js/auth.js
@@ -15,4 +15,29 @@ function login() {
       document.getElementById("msg").innerText = d.message;
     }
   });
+
+  router.get('/profile',(req, res) => {
+    const session = req.cookies?.session;
+
+    if(!session){
+      return res.status(401).json({message:"no session"});
+    }
+
+    const sql = `select id, email from users where session_token =?`;
+
+    db.query(sql,[session],(err,result) => {
+      if(err){
+        return res.status(500).json({message:"server error"});
+      }
+
+      if(result.length ===0){
+        return res.status(401).json({ message:"invalid session"});
+    }
+
+      res.json({
+        message:"profile accessed",
+        user: result[0]
+      });
+    });
+  });
 }
